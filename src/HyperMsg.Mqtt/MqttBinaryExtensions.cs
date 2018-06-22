e52e60cfeac0fd8c63f20283e260df6b260d3543
Maxim Chasnik <maxim.chasnik@gmail.com>
2018-06-22 16:02:54 +0300

    Added packet serialization

--- src/HyperMsg.Mqtt/MqttBinaryExtensions.cs
+++ src/HyperMsg.Mqtt/MqttBinaryExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace HyperMsg.Mqtt
 {
@@ -16,6 +18,12 @@ namespace HyperMsg.Mqtt
 		static MqttBinaryExtensions()
 		{
 			writers = new Dictionary<Type, Func<Memory<byte>, Packet, int>>();
+			AddWriter<PubAck>(Write);
+			AddWriter<PubRec>(Write);
+			AddWriter<PubRel>(Write);
+			AddWriter<PubComp>(Write);
+			AddWriter<Subscribe>(Write);
+			AddWriter<SubAck>(Write);
 			AddWriter<Unsubscribe>(Write);
 			AddWriter<UnsubAck>(Write);
 			AddWriter<PingReq>(Write);
@@ -28,31 +36,94 @@ namespace HyperMsg.Mqtt
 			throw new NotImplementedException();
 		}
 
-		public static int WritePacket(this Memory<byte> buffer, Packet packet)
+		public static int ReadRemainingLength(this ReadOnlyMemory<byte> buffer)
 		{
-			return writers[packet.GetType()](buffer, packet);
+			throw new NotImplementedException();
+		}
+
+		public static string ReadString(this ReadOnlyMemory<byte> buffer)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static int WritePacket(this Memory<byte> buffer, Packet packet) => writers[packet.GetType()](buffer, packet);
+
+		private static int Write(Memory<byte> buffer, PubAck packet) => WriteShortPacket(buffer, PacketCodes.Puback, packet.Id);
+
+		private static int Write(Memory<byte> buffer, PubRec packet) => WriteShortPacket(buffer, PacketCodes.Pubrec, packet.Id);
+
+		private static int Write(Memory<byte> buffer, PubRel packet) => WriteShortPacket(buffer, PacketCodes.Pubrel, packet.Id);
+
+		private static int Write(Memory<byte> buffer, PubComp packet) => WriteShortPacket(buffer, PacketCodes.Pubcomp, packet.Id);
+
+		private static int Write(Memory<byte> buffer, Subscribe packet)
+		{
+			var span = buffer.Span;
+
+			span[0] = PacketCodes.Subscribe;
+
+			int contentLength = GetSubscriptionsByteCount(packet.Subscriptions) + sizeof(ushort);
+			int offset = buffer.WriteRemainingLength(contentLength, 1) + 1;
+			buffer.WriteUInt16BigEndian(packet.Id, offset);
+			offset += sizeof(ushort);
+
+			foreach (var (topic, qos) in packet.Subscriptions)
+			{
+				offset += buffer.WriteString(topic, offset);
+				span[offset] = (byte)qos;
+				offset++;
+			}
+
+			return offset;
+		}
+
+		private static int GetSubscriptionsByteCount((string, QosLevel)[] subscriptions) => subscriptions.Aggregate(0, (a, s) => a + GetStringByteCount(s.Item1) + 1);
+
+		private static int Write(Memory<byte> buffer, SubAck packet)
+		{
+			var span = buffer.Span;
+
+			span[0] = PacketCodes.SubAck;
+
+			int contentLength = packet.Results.Length + sizeof(ushort);
+			int offset = buffer.WriteRemainingLength(contentLength, 1) + 1;
+			buffer.WriteUInt16BigEndian(packet.Id, offset);
+			offset += sizeof(ushort);
+
+			foreach (var result in packet.Results)
+			{
+				span[offset] = (byte)result;
+				offset++;
+			}
+
+			return offset;
 		}
 
 		private static int Write(Memory<byte> buffer, Unsubscribe packet)
 		{
 			var span = buffer.Span;
-			buffer.WriteUInt16BigEndian(packet.Id, 0);
-
-			//foreach (var filter in packet.Topics)
-			//{
-			//	buffer.WriteString(filter);
-			//}
 
 			span[0] = PacketCodes.Unsubscribe;
-			//buffer.WriteRemainingLength(_buffer.Length);
+			var offset = 1;
+			var contentLength = GetTopicsByteCount(packet.Topics) + sizeof(ushort);
+			offset += buffer.WriteRemainingLength(contentLength, offset);
 
-			return -1;
+			buffer.WriteUInt16BigEndian(packet.Id, offset);
+			offset += sizeof(ushort);
+
+			foreach (var filter in packet.Topics)
+			{
+				offset += buffer.WriteString(filter, offset);
+			}
+
+			return offset;
 		}
 
-		private static int Write(Memory<byte> buffer, UnsubAck packet)
-		{
-			return WriteShortPacket(buffer, PacketCodes.UnsubAck, packet.Id);
-		}
+		private static int GetTopicsByteCount(string[] topics) => topics.Aggregate(0, (a, s) => a + GetStringByteCount(s));
+
+		private static int GetStringByteCount(string str) => Encoding.UTF8.GetByteCount(str) + sizeof(ushort);
+
+		private static int Write(Memory<byte> buffer, UnsubAck packet) => WriteShortPacket(buffer, PacketCodes.UnsubAck, packet.Id);
 
 		private static int WriteShortPacket(Memory<byte> buffer, byte code, ushort packetId)
 		{
@@ -82,9 +153,46 @@ namespace HyperMsg.Mqtt
 			return Disconnect.Length;
 		}
 
-		private static void AddWriter<T>(Func<Memory<byte>, T, int> writer) where T : Packet
+		public static int WriteRemainingLength(this Memory<byte> buffer, int length, int offset)
 		{
-			writers.Add(typeof(T), (w, p) => writer(w, (T)p));
+			var span = buffer.Span;
+
+			if (length > 0x1fffff)
+			{
+				span[offset] = (byte)(length | 0x80);
+				span[offset + 1] = (byte)((length >> 7) | 0x80);
+				span[offset + 2] = (byte)((length >> 14) | 0x80);
+				span[offset + 3] = (byte)(length >> 21);
+				return 4;
+			}
+
+			if (length > 0x3fff)
+			{
+				span[0] = (byte)(length | 0x80);
+				span[0 + 1] = (byte)((length >> 7) | 0x80);
+				span[0 + 2] = (byte)(length >> 14);
+				return 3;
+			}
+
+			if (length > 0x7f)
+			{
+				span[0] = (byte)(length | 0x80);
+				span[0 + 1] = (byte)(length >> 7);
+				return 2;
+			}
+
+			span[offset] = (byte) length;
+			return 1;
 		}
+
+		public static int WriteString(this Memory<byte> buffer, string value, int offset)
+		{
+			var bytes = Encoding.UTF8.GetBytes(value);
+			buffer.WriteUInt16BigEndian((ushort)bytes.Length, offset);
+			bytes.CopyTo(buffer.Slice(offset + sizeof(ushort)));
+			return bytes.Length + sizeof(ushort);
+		}
+
+		private static void AddWriter<T>(Func<Memory<byte>, T, int> writer) where T : Packet => writers.Add(typeof(T), (w, p) => writer(w, (T)p));
 	}
 }
--- test/HyperMsg.Mqtt.Tests/MqttBinaryExtensionTests.cs
+++ test/HyperMsg.Mqtt.Tests/MqttBinaryExtensionTests.cs
@@ -1,11 +1,107 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Xunit;
 
 namespace HyperMsg.Mqtt.Tests
 {
     public class MqttBinaryExtensionTests
     {
-	    [Fact(DisplayName = "WritePacket serializes Unsubscribe packet")]
+	    [Fact(DisplayName = "WritePacket serializes PubAck packet")]
+	    public void WritePacket_Serializes_Puback_Packet()
+	    {
+		    ushort packetId = 0x8178;
+		    var packet = new PubAck(packetId);
+		    byte[] expected =
+		    {
+			    0b01000000, //Type code
+			    2, //Length
+			    0x81, 0x78 //Packet ID
+		    };
+
+			VerifySerialization(packet, expected);
+		}
+
+	    [Fact(DisplayName = "WritePacket serializes PubRec packet")]
+	    public void WritePacket_Serializes_Pubrec_Packet()
+	    {
+		    ushort packetId = 0x8179;
+		    var packet = new PubRec(packetId);
+		    byte[] expected =
+		    {
+			    0b01010000, //Type code
+			    2, //Length
+			    0x81, 0x79 //Packet ID
+		    };
+
+			VerifySerialization(packet, expected);
+		}
+
+	    [Fact(DisplayName = "WritePacket serializes PubRel packet")]
+	    public void WritePacket_Serializes_Pubrel_Packet()
+	    {
+		    ushort packetId = 0x8079;
+		    var packet = new PubRel(packetId);
+		    byte[] expected =
+		    {
+			    0b01100010, //Type code
+			    2, //Length
+			    0x80, 0x79 //Packet ID
+		    };
+
+			VerifySerialization(packet, expected);
+		}
+
+		[Fact(DisplayName = "WritePacket serialzies PubComp packet")]
+	    public void WritePacket_Serializes_Pubcomp_Packet()
+	    {
+		    ushort packetId = 0x8989;
+		    var packet = new PubComp(packetId);
+		    byte[] expected =
+		    {
+			    0b01110000, //Type code
+			    2, //Length
+			    0x89, 0x89 //Packet ID
+		    };
+
+		    VerifySerialization(packet, expected);
+	    }
+
+		[Fact(DisplayName = "WritePacket serializes Subscribe packet")]
+	    public void WritePacket_Serializes_Subscribe_Packet()
+	    {
+		    ushort packetId = 0x8098;
+		    var packet = new Subscribe(packetId, ("a/b", QosLevel.Qos1), ("c/d", QosLevel.Qos2));
+		    byte[] expected =
+		    {
+			    0b10000010, //Type code
+			    14, //Length
+			    0x80, 0x98, //Packet ID
+			    0, 3, 0x61, 0x2f, 0x62, 1, //Filter "a/b" Qos1
+			    0, 3, 0x63, 0x2f, 0x64, 2 //Filter "c/d" Qos2
+		    };
+
+		    VerifySerialization(packet, expected);
+	    }
+
+		[Fact(DisplayName = "WritePacket serializes SubAck packet")]
+	    public void WritePacket_Serializes_SubAck_Packet()
+	    {
+		    ushort packetId = 0x6790;
+		    var packet = new SubAck(packetId, SubscriptionResult.SuccessQos0, SubscriptionResult.SuccessQos2, SubscriptionResult.Failure);
+		    byte[] expected =
+		    {
+			    0b10010000, //Type code
+			    5, //Length
+			    0x67, 0x90, //Packet ID
+			    0, 0x02, 0x80 //Response codes
+		    };
+
+		    VerifySerialization(packet, expected);
+	    }
+
+		[Fact(DisplayName = "WritePacket serializes Unsubscribe packet")]
 	    public void WritePacket_Serializes_Unsubscribe_Packet()
 	    {
 		    ushort packetId = 0x0c1d;
@@ -52,8 +148,51 @@ namespace HyperMsg.Mqtt.Tests
 		    var buffer = new Memory<byte>(new byte[expected.Length]);
 		    int written = buffer.WritePacket(packet);
 
-			Assert.Equal(expected.Length, written);
-			Assert.Equal(expected, buffer.ToArray());
+		    Assert.Equal(expected, buffer.ToArray());
+		    Assert.Equal(expected.Length, written);
 	    }
-    }
+
+	    public static IEnumerable<object[]> GetTestCasesForWriteRemaningLength()
+	    {
+		    yield return GetTestCaseForWriteRemainingLength(0, 0);
+		    yield return GetTestCaseForWriteRemainingLength(127, 0x7f);
+		    yield return GetTestCaseForWriteRemainingLength(128, 0x80, 0x01);
+		    yield return GetTestCaseForWriteRemainingLength(16383, 0xFF, 0x7F);
+		    yield return GetTestCaseForWriteRemainingLength(16384, 0x80, 0x80, 0x01);
+		    yield return GetTestCaseForWriteRemainingLength(2097151, 0xFF, 0xFF, 0x7F);
+		    yield return GetTestCaseForWriteRemainingLength(2097152, 0x80, 0x80, 0x80, 0x01);
+		    yield return GetTestCaseForWriteRemainingLength(268435455, 0xFF, 0xFF, 0xFF, 0x7F);
+	    }
+
+	    public static object[] GetTestCaseForWriteRemainingLength(int value, params byte[] expected)
+	    {
+		    return new object[] { value, expected };
+	    }
+
+	    [Theory(DisplayName = "WriteRemainingLength serializes value for remaining length")]
+	    [MemberData(nameof(GetTestCasesForWriteRemaningLength))]
+	    public void WriteRemainingLength_Serializes_Value_For_Remaining_Length(int value, byte[] expected)
+	    {
+		    var buffer = new Memory<byte>(new byte[expected.Length]);
+
+		    int bytesWritten = buffer.WriteRemainingLength(value, 0);
+
+		    Assert.Equal(expected.Length, bytesWritten);
+		    Assert.Equal(expected, buffer.ToArray());
+	    }
+
+	    [Fact(DisplayName = "WriteString correctly serializes string")]
+	    public void WriteString_Correctly_Serializes_String()
+	    {
+		    string value = Guid.NewGuid().ToString();
+		    byte[] expected = new byte[] { 0, (byte)value.Length }.Concat(Encoding.UTF8.GetBytes(value)).ToArray();
+			var buffer = new Memory<byte>(expected);
+
+		    var bytesWritten = buffer.WriteString(value, 0);
+		    byte[] actual = buffer.ToArray();
+
+			Assert.Equal(expected.Length, bytesWritten);
+		    Assert.Equal(expected, actual);
+	    }
+	}
 }
