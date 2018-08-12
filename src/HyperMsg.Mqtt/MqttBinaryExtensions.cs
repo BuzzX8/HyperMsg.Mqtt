5e7d58a30993cfd07d8da524779b37254fe0debc
Maxim Chasnik <maxim.chasnik@outlook.com>
2018-08-12 20:48:55 +0300

    Added serialization for Connect and Publish

--- src/HyperMsg.Mqtt/MqttBinaryExtensions.cs
+++ src/HyperMsg.Mqtt/MqttBinaryExtensions.cs
@@ -10,14 +10,19 @@ namespace HyperMsg.Mqtt
 		private static readonly byte[] Disconnect = { 0b11100000, 0b00000000 };
 		private static readonly byte[] PingResp = { 0b11010000, 0b00000000 };
 		private static readonly byte[] PingReq = { 0b11000000, 0b00000000 };
+        private static readonly byte[] ProtocolName = { (byte)'M', (byte)'Q', (byte)'T', (byte)'T' };
 
 		const byte ConnectCode = 0b00010000;
+
 
 		private static readonly Dictionary<Type, Func<Memory<byte>, Packet, int>> writers;
 
 		static MqttBinaryExtensions()
 		{
 			writers = new Dictionary<Type, Func<Memory<byte>, Packet, int>>();
+            AddWriter<Connect>(Write);
+            AddWriter<ConnAck>(Write);
+            AddWriter<Publish>(Write);
 			AddWriter<PubAck>(Write);
 			AddWriter<PubRec>(Write);
 			AddWriter<PubRel>(Write);
@@ -38,15 +43,128 @@ namespace HyperMsg.Mqtt
 
 		public static int ReadRemainingLength(this ReadOnlyMemory<byte> buffer)
 		{
+            /*
+             * int result = 0;
+			int value = 0;
+			int offset = 0;
+			int i = 0;
+
+			do
+			{
+				i++;
+				value = reader.ReadByte();
+
+				if (i == sizeof(int) && value >= 0x80)
+				{
+					throw new DeserializationException();
+				}
+
+				result |= (value & 0x7f) << offset;
+				offset += 7;
+			}
+			while((value & 0x80) == 0x80);
+			return result;*/
 			throw new NotImplementedException();
 		}
 
 		public static string ReadString(this ReadOnlyMemory<byte> buffer)
 		{
+            /*
+             * ushort length = reader.ReadUInt16();
+		    var bytes = reader.ReadBytes(length);
+		    return Encoding.UTF8.GetString(bytes, 0, bytes.Length);*/
 			throw new NotImplementedException();
 		}
 
 		public static int WritePacket(this Memory<byte> buffer, Packet packet) => writers[packet.GetType()](buffer, packet);
+
+        private static int Write(Memory<byte> buffer, Connect packet)
+        {
+            var span = buffer.Span;
+
+            span[0] = ConnectCode;
+            int offset = 1;
+            int contentLength = 104;
+            offset += WriteProtocolName(buffer, offset);
+
+            offset += buffer.WriteRemainingLength(contentLength, offset);
+            span[offset] = 4;//Protocol level
+            span[offset + 1] = (byte)packet.Flags;
+            offset += 2;
+            buffer.WriteUInt16BigEndian(packet.KeepAlive, offset);
+            //_bufferWriter.WriteString(packet.ClientId);
+
+            //if (packet.Flags.HasFlag(ConnectFlags.Will))
+            //{
+            //    _bufferWriter.WriteString(packet.WillTopic);
+            //    _bufferWriter.Write((ushort)packet.WillMessage.Length);
+            //    _bufferWriter.Write(packet.WillMessage, 0, packet.WillMessage.Length);
+            //}
+
+            //if (packet.Flags.HasFlag(ConnectFlags.UserName))
+            //{
+            //    _bufferWriter.WriteString(packet.UserName);
+            //}
+
+            //if (packet.Flags.HasFlag(ConnectFlags.Password))
+            //{
+            //    _bufferWriter.Write((ushort)packet.Password.Length);
+            //    _bufferWriter.Write(packet.Password, 0, packet.Password.Length);
+            //}
+
+            return offset;
+        }
+
+        private static int WriteProtocolName(Memory<byte> buffer, int offset)
+        {
+            var span = buffer.Span;
+            buffer.WriteUInt16BigEndian(0x0004, offset);
+            ProtocolName.CopyTo(buffer.Slice(sizeof(ushort) + offset));
+
+            return sizeof(ushort) + 4;
+        }
+
+        private static int Write(Memory<byte> buffer, ConnAck packet)
+        {
+            var span = buffer.Span;
+
+            span[0] = 0x20;
+            span[1] = 0x02;
+            span[2] = (byte)(packet.SessionPresent ? 1 : 0);
+            span[3] = (byte)packet.ResultCode;
+
+            return 4;
+        }
+
+        private static int Write(Memory<byte> buffer, Publish packet)
+        {
+            byte header = 0b00110000;
+            var span = buffer.Span;
+
+            if (packet.Dup)
+            {
+                header |= 0x08;
+            }
+
+            header |= (byte)((byte)packet.Qos << 1);
+
+            if (packet.Retain)
+            {
+                header |= 0x01;
+            }
+
+            span[0] = header;
+            int offset = 1;
+            int contentLength = GetStringByteCount(packet.Topic) + sizeof(ushort) + packet.Message.Length;
+            offset += buffer.WriteRemainingLength(contentLength, offset);
+
+            offset += buffer.WriteString(packet.Topic, offset);
+            buffer.WriteUInt16BigEndian(packet.Id, offset);
+            offset += sizeof(ushort);
+            packet.Message.CopyTo(span.Slice(offset));
+            
+            return offset + packet.Message.Length;
+        }
 
 		private static int Write(Memory<byte> buffer, PubAck packet) => WriteShortPacket(buffer, PacketCodes.Puback, packet.Id);
 
--- test/HyperMsg.Mqtt.Tests/MqttBinaryExtensionTests.cs
+++ test/HyperMsg.Mqtt.Tests/MqttBinaryExtensionTests.cs
@@ -8,7 +8,189 @@ namespace HyperMsg.Mqtt.Tests
 {
     public class MqttBinaryExtensionTests
     {
-	    [Fact(DisplayName = "WritePacket serializes PubAck packet")]
+        [Fact(DisplayName = "Serializes Connect packet with Password flag")]
+        public void Serializes_Connect_With_Password_Flag()
+        {
+            byte[] password = Guid.NewGuid().ToByteArray();
+            var packet = new Connect
+            {
+                Flags = ConnectFlags.Password,
+                ClientId = Guid.NewGuid().ToString(),
+                Password = password
+            };
+            var expected = CreateConnectHeader(packet);
+            AddBinary(expected, password);
+            SetRemainingLength(expected);
+
+            VerifySerialization(packet, expected.ToArray());
+        }
+
+        [Fact(DisplayName = "Serializes Connect with Username flag")]
+        public void Serializes_Connect_With_Username_Flag()
+        {
+            string username = Guid.NewGuid().ToString();
+            var packet = new Connect
+            {
+                ClientId = Guid.NewGuid().ToString(),
+                Flags = ConnectFlags.UserName,
+                UserName = username
+            };
+            var expected = CreateConnectHeader(packet);
+            AddString(expected, username);
+            SetRemainingLength(expected);
+
+            VerifySerialization(packet, expected.ToArray());
+        }
+
+        [Fact(DisplayName = "Serializes Connect with Will flag")]
+        public void Serializes_Connect_With_Will_Flag()
+        {
+            string willTopic = Guid.NewGuid().ToString();
+            byte[] willMessage = Guid.NewGuid().ToByteArray();
+            var packet = new Connect
+            {
+                ClientId = Guid.NewGuid().ToString(),
+                Flags = ConnectFlags.Will,
+                WillTopic = willTopic,
+                WillMessage = willMessage
+            };
+            var expected = CreateConnectHeader(packet);
+            AddString(expected, willTopic);
+            AddBinary(expected, willMessage);
+            SetRemainingLength(expected);
+
+            VerifySerialization(packet, expected.ToArray());
+        }
+
+        public static IEnumerable<object[]> GetTestCasesForSerializeConnect()
+        {
+            yield return new object[] { ConnectFlags.None };
+            yield return new object[] { ConnectFlags.CleanSession };
+            yield return new object[] { ConnectFlags.WillRetain };
+            yield return new object[] { ConnectFlags.Qos0 };
+            yield return new object[] { ConnectFlags.Qos1 };
+            yield return new object[] { ConnectFlags.Qos2 };
+
+            yield return new object[] { ConnectFlags.CleanSession | ConnectFlags.WillRetain };
+        }
+
+        [Theory(DisplayName = "Serializes Connect packet")]
+        [MemberData(nameof(GetTestCasesForSerializeConnect))]
+        public void Serializes_Connect(ConnectFlags flags)
+        {
+            ushort keepAlive = BitConverter.ToUInt16(Guid.NewGuid().ToByteArray(), 0);
+            string clientId = Guid.NewGuid().ToString();
+            var packet = new Connect
+            {
+                Flags = flags,
+                KeepAlive = keepAlive,
+                ClientId = clientId
+            };
+            var expected = CreateConnectHeader(packet);
+            SetRemainingLength(expected);
+
+            VerifySerialization(packet, expected.ToArray());
+        }
+
+        private static List<byte> CreateConnectHeader(Connect packet)
+        {
+            List<byte> expected = new List<byte>
+            {
+                0x10, //Type code
+				0, //Length placeholder
+				0, 4, (byte)'M', (byte)'Q', (byte)'T', (byte)'T', //Protocol name
+				4, //Protocol level
+				(byte)packet.Flags, //Flags
+				(byte)(packet.KeepAlive >> 8), (byte)packet.KeepAlive,
+				//0, (byte)packet.ClientId.Length//Client ID length
+			};
+            AddString(expected, packet.ClientId);
+            return expected;
+        }
+
+        private static void AddBinary(List<byte> packet, byte[] bytes)
+        {
+            packet.AddRange(new byte[] { 0, (byte)bytes.Length });
+            packet.AddRange(bytes);
+        }
+
+        private static void AddString(List<byte> packet, string str)
+        {
+            packet.AddRange(new byte[] { 0, (byte)str.Length });
+            packet.AddRange(Encoding.UTF8.GetBytes(str));
+        }
+
+        private static void SetRemainingLength(List<byte> packet)
+        {
+            packet[1] = (byte)(packet.Count - 2);
+        }
+
+        public static IEnumerable<object[]> GetTestCasesForSerializeConnAck()
+        {
+            yield return new object[] { false, ConnectionResult.Accepted };
+            yield return new object[] { true, ConnectionResult.BadUsernameOrPassword };
+            yield return new object[] { false, ConnectionResult.IdentifierRejected };
+            yield return new object[] { true, ConnectionResult.NotAuthorized };
+            yield return new object[] { false, ConnectionResult.ServerUnavailable };
+            yield return new object[] { true, ConnectionResult.UnacceptableVersion };
+        }
+
+        [Theory(DisplayName = "Serializes ConnAck packet")]
+        [MemberData(nameof(GetTestCasesForSerializeConnAck))]
+        public void Serializes_ConnAck_Packet(bool sessionPresent, ConnectionResult result)
+        {
+            var packet = new ConnAck(result, sessionPresent);
+            byte[] expected =
+            {
+                0b00100000, //packet code
+				0x02, //length
+				(byte)(sessionPresent ? 0x01 : 0x00), //flags
+				(byte)result
+            };
+
+            VerifySerialization(packet, expected);
+        }
+
+        public static IEnumerable<object[]> GetTestCasesForSerializePublish()
+        {
+            yield return new object[] { 0b00110000, false, QosLevel.Qos0, false };
+            yield return new object[] { 0b00111000, true, QosLevel.Qos0, false };
+            yield return new object[] { 0b00111010, true, QosLevel.Qos1, false };
+            yield return new object[] { 0b00111100, true, QosLevel.Qos2, false };
+            yield return new object[] { 0b00111011, true, QosLevel.Qos1, true };
+        }
+
+        [Theory(DisplayName = "WritePacket serializes Publish packet")]
+        [MemberData(nameof(GetTestCasesForSerializePublish))]
+        public void WritePacket_Serializes_Publish_Packet(byte expectedHeader, bool dup, QosLevel qos, bool retain)
+        {
+            ushort packetId = 0x8667;
+            string topicName = Guid.NewGuid().ToString();
+            byte[] payload = Guid.NewGuid().ToByteArray();
+
+            var packet = new Publish(packetId)
+            {
+                Dup = dup,
+                Qos = qos,
+                Retain = retain,
+                Topic = topicName,
+                Message = payload
+            };
+            List<byte> expected = new List<byte>
+            {
+                expectedHeader, //Packet type, dup flag, retain
+				(byte)(topicName.Length + 2 /*topic length*/ + 2 /*packet ID*/ + payload.Length), //Length
+				0x00, (byte)topicName.Length
+            };
+            expected.AddRange(Encoding.UTF8.GetBytes(topicName));
+            expected.Add((byte)(packetId >> 8));
+            expected.Add((byte)packetId);
+            expected.AddRange(payload);
+
+            VerifySerialization(packet, expected.ToArray());
+        }
+
+        [Fact(DisplayName = "WritePacket serializes PubAck packet")]
 	    public void WritePacket_Serializes_Puback_Packet()
 	    {
 		    ushort packetId = 0x8178;
