c7e1d34e8a31abf34b371fb1ac85907e341728b2
Maxim Chasnik <maksym.c@sbtech.com>
2019-05-14 17:27:09 +0300

    added config extensions

--- src/HyperMsg.Mqtt.Client/ConfigurableExtensions.cs
+++ src/HyperMsg.Mqtt.Client/ConfigurableExtensions.cs
@@ -4,22 +4,21 @@
     {
         public static void UseMqttClient(this IConfigurable configurable, MqttConnectionSettings connectionSettings)
         {
+            configurable.AddSetting(nameof(MqttConnectionSettings), connectionSettings);
             configurable.Configure(c =>
             {
-                var descriptor = ServiceDescriptor.Describe(typeof(IMqttClient), provider =>
-                {
-                    var sender = (ISender<Packet>)provider.GetService(typeof(ISender<Packet>));
-                    var transportHandler = (IHandler<TransportCommands>)provider.GetService(typeof(IHandler<TransportCommands>));
-                    var receiveModeHandler = (IHandler<ReceiveMode>)provider.GetService(typeof(IHandler<ReceiveMode>));
-                    var settings = (MqttConnectionSettings)c.Settings[nameof(MqttConnectionSettings)];
+                var sender = (ISender<Packet>)c.GetService(typeof(ISender<Packet>));
+                var transportHandler = (IHandler<TransportCommands>)c.GetService(typeof(IHandler<TransportCommands>));
+                var receiveModeHandler = (IHandler<ReceiveMode>)c.GetService(typeof(IHandler<ReceiveMode>));
+                var settings = (MqttConnectionSettings)c.GetSetting(nameof(MqttConnectionSettings));
 
-                    return new MqttClient(sender, settings)
-                    {
-                        SetReceiveModeAsync = receiveModeHandler.HandleAsync,
-                        SubmitTransportCommandAsync = transportHandler.HandleAsync
-                    };
-                });
-                c.Services.Add(descriptor);
+                var client = new MqttClient(sender, settings)
+                {
+                    SetReceiveModeAsync = receiveModeHandler.HandleAsync,
+                    SubmitTransportCommandAsync = transportHandler.HandleAsync
+                };
+
+                c.RegisterService(typeof(IMqttClient), client);
             });
         }
     }
---
+++ src/HyperMsg.Mqtt.Serialization/ConfigurableExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace HyperMsg.Mqtt.Serialization
+{
+    public static class ConfigurableExtensions
+    {
+        public static void UseMqttSerializer(IConfigurable configurable)
+        {
+            configurable.Configure(context =>
+            {
+                context.RegisterService(typeof(ISerializer<Packet>), new MqttSerializer());
+            });
+        }
+    }
+}
