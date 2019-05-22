68e1e9b7a3115d08f0efff5cc9d0bb5d8e145f2c
Maxim Chasnik <maksym.c@sbtech.com>
2019-05-22 17:27:19 +0300

    wip

---
+++ test/HyperMsg.Mqtt.Integration.Tests/MqttClientTests.cs
@@ -0,0 +1,28 @@
+﻿using HyperMsg.Mqtt.Client;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace HyperMsg.Mqtt.Integration
+{
+    public class MqttClientTests
+    {
+        private readonly IMqttClient client;
+        private readonly List<Packet> receivedPackets;
+
+        public MqttClientTests()
+        {
+            receivedPackets = new List<Packet>();
+        }
+        
+        [Fact]
+        public void Connect_Receives_ConnAck_From_Server()
+        {
+            client.Connect(true);
+
+            var response = receivedPackets.SingleOrDefault() as ConnAck;
+
+            Assert.NotNull(response);
+        }
+    }
+}
