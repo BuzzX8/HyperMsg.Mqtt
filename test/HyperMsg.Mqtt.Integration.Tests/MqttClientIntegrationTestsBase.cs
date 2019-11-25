3b7d6c9e3dcda6a9dec19429ce8844bffba057e1
Maxim Chasnik <maksym.c@sbtech.com>
2019-11-25 13:29:09 +0200

    wip

--- test/HyperMsg.Mqtt.Integration.Tests/MqttClientIntegrationTestsBase.cs
+++ test/HyperMsg.Mqtt.Integration.Tests/MqttClientIntegrationTestsBase.cs
@@ -1,9 +1,11 @@
 ﻿using HyperMsg.Integration;
 using HyperMsg.Mqtt.Client;
 using HyperMsg.Mqtt.Serialization;
+using HyperMsg.Transport.Socket;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -11,18 +13,22 @@ using Xunit;
 namespace HyperMsg.Mqtt.Integration
 {
     [Collection("Integration")]
-    public abstract class MqttClientIntegrationTestsBase : SocketTransportFixtureBase<Packet>, IDisposable
+    public abstract class MqttClientIntegrationTestsBase : IntegrationFixtureBase, IDisposable
     {
         const int MqttPort = 1883;
 
         protected readonly string ClientId = Guid.NewGuid().ToString();
         private readonly List<Packet> responses;
 
-        public MqttClientIntegrationTestsBase() : base(MqttPort)
+        public MqttClientIntegrationTestsBase()
         {
             ConnectionSettings = new MqttConnectionSettings(ClientId);
             responses = new List<Packet>();
-            HandlerRegistry.Register(p => responses.Add(p));
+            Configurable.UseCoreServices(2048, 2048);
+            Configurable.UseSockets(new IPEndPoint(IPAddress.Loopback, MqttPort));
+            Configurable.UseMqttSerialization();
+            Configurable.UseMqttClient(ConnectionSettings);
+            HandlerRegistry.Register<Received<Packet>>(p => responses.Add(p.Message));
         }
 
         protected IMqttClient Client => GetService<IMqttClient>();
@@ -33,18 +39,20 @@ namespace HyperMsg.Mqtt.Integration
 
         protected MqttConnectionSettings ConnectionSettings { get; }
 
-        protected override void ConfigureSerializer(IConfigurable configurable) => configurable.UseMqttSerializer();
-
-        protected override void ConfigureServices(IConfigurable configurable)
-        {
-            base.ConfigureServices(configurable);
-            configurable.UseMqttClient(ConnectionSettings);
-        }
-
         protected Task<SessionState> ConnectAsync(bool cleanSession, CancellationToken cancellationToken) => Client.ConnectAsync(cleanSession, cancellationToken);
 
         protected Task DisconnectAsync(CancellationToken cancellationToken) => Client.DisconnectAsync(cancellationToken);
 
         public void Dispose() => DisconnectAsync(default).Wait();
+
+        protected override void ConfigureSerializer(IConfigurable configurable)
+        {
+            
+        }
+
+        protected override void ConfigureTransport(IConfigurable configurable)
+        {
+            
+        }
     }
 }
