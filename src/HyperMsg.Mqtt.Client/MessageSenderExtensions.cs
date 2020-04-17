916d7b09ee05d3ce17ec3567f2184c41c40edbb0
BuzzX8 <maxim.chasnik@gmail.com>
2020-04-17 19:36:48 +0300

    wip

--- src/HyperMsg.Mqtt.Client/MessageSenderExtensions.cs
+++ src/HyperMsg.Mqtt.Client/MessageSenderExtensions.cs
@@ -1,11 +1,14 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HyperMsg.Mqtt.Client
 {
     public static class MessageSenderExtensions
     {
-        public static Task SendConnectRequestAsync(this IMessageSender messageSender, MqttConnectionSettings connectionSettings, CancellationToken cancellationToken)
+        public static Task TransmitConnectAsync(this IMessageSender messageSender, MqttConnectionSettings connectionSettings, CancellationToken cancellationToken)
         {
             var packet = CreateConnectPacket(connectionSettings);
             return messageSender.TransmitAsync(packet, cancellationToken);
@@ -35,6 +38,38 @@ namespace HyperMsg.Mqtt.Client
             }
 
             return connect;
+        }
+
+        public static Task TransmitDisconnectAsync(this IMessageSender messageSender, CancellationToken cancellationToken)
+        {
+            return messageSender.TransmitAsync(Disconnect.Instance, cancellationToken);
+        }
+
+        public static Task TransmitSubscribeAsync(this IMessageSender messageSender, ushort packetId, IEnumerable<(string, QosLevel)> requests, CancellationToken cancellationToken)
+        {
+            var request = new Subscribe(packetId, requests);
+            return messageSender.TransmitAsync(request, cancellationToken);
+        }
+
+        public static Task TransmitUnsubscribeAsync(this IMessageSender messageSender, ushort packetId, IEnumerable<string> topics, CancellationToken cancellationToken)
+        {
+            var request = new Unsubscribe(packetId, topics);
+            return messageSender.TransmitAsync(request, cancellationToken);
+        }
+
+        public static Task TransmitPublishAsync(this IMessageSender messageSender, ushort packetId, string topic, QosLevel qosLevel, ReadOnlyMemory<byte> message, bool retain, bool dup, CancellationToken cancellationToken)
+        {
+            var publish = new Publish(packetId, topic, message, qosLevel)
+            {
+                Dup = dup,
+                Retain = retain
+            };
+            return messageSender.TransmitAsync(publish, cancellationToken);
+        }
+
+        public static Task TransmitPingReqAsync(this IMessageSender messageSender, CancellationToken cancellationToken)
+        {
+            return messageSender.TransmitAsync(PingReq.Instance, cancellationToken);
         }
     }
 }
--- test/HyperMsg.Mqtt.Client.Tests/MessageSenderExtensionsTests.cs
+++ test/HyperMsg.Mqtt.Client.Tests/MessageSenderExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using FakeItEasy;
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -12,7 +13,7 @@ namespace HyperMsg.Mqtt.Client
         private readonly MqttConnectionSettings connectionSettings = new MqttConnectionSettings(Guid.NewGuid().ToString());
 
         [Fact]
-        public async Task SendConnectRequestAsync_Sends_Connect_Packet()
+        public async Task TransmitConnectAsync_Transmits_Connect_Packet()
         {
             var expectedMessage = new Connect
             {
@@ -23,7 +24,7 @@ namespace HyperMsg.Mqtt.Client
         }
 
         [Fact]
-        public async Task SendConnectRequestAsync_Sends_Connect_Packet_With_CleanSession_Flag()
+        public async Task TransmitConnectAsync_Transmits_Connect_Packet_With_CleanSession_Flag()
         {
             var expectedMessage = new Connect
             {
@@ -36,7 +37,7 @@ namespace HyperMsg.Mqtt.Client
         }
 
         [Fact]
-        public async Task SendConnectRequestAsync_Sends_Connect_Packet_With_KeepAlive_Specified_In_Settings()
+        public async Task TransmitConnectAsync_Transmits_Connect_Packet_With_KeepAlive_Specified_In_Settings()
         {
             connectionSettings.KeepAlive = 0x9080;
             var expectedMessage = new Connect
@@ -49,7 +50,7 @@ namespace HyperMsg.Mqtt.Client
         }
 
         [Fact]
-        public async Task SendConnectRequestAsync_Sends_Connect_Packet_With_Corredt_WillMessageSettings()
+        public async Task TransmitConnectAsync_Transmits_Connect_Packet_With_Corredt_WillMessageSettings()
         {
             var willTopic = Guid.NewGuid().ToString();
             var willMessage = Guid.NewGuid().ToByteArray();
@@ -67,9 +68,61 @@ namespace HyperMsg.Mqtt.Client
 
         private async Task VerifySendConnectRequestAsync(Connect expected, MqttConnectionSettings connectionSettings)
         {
-            await messageSender.SendConnectRequestAsync(connectionSettings, default);
+            await messageSender.TransmitConnectAsync(connectionSettings, default);
 
             A.CallTo(() => messageSender.SendAsync(new Transmit<Connect>(expected), A<CancellationToken>._)).MustHaveHappened();
+        }
+
+        [Fact]
+        public async Task TransmitDisconnectAsync_Transmits_Correct_Packet()
+        {
+            await messageSender.TransmitDisconnectAsync(default);
+
+            A.CallTo(() => messageSender.SendAsync(new Transmit<Disconnect>(Disconnect.Instance), default)).MustHaveHappened();
+        }
+
+        [Fact]
+        public async Task TransmitSubscribeAsync_Transmits_Correct_Subscribe_Request()
+        {
+            var request = Enumerable.Range(1, 5)
+                .Select(i => ($"topic-{i}", (QosLevel)(i % 3)))
+                .ToArray();
+            var packetId = (ushort)0x8888;
+            var expectedMessage = new Subscribe(packetId, request);
+
+            await messageSender.TransmitSubscribeAsync(packetId, request, default);
+
+            A.CallTo(() => messageSender.SendAsync(new Transmit<Subscribe>(expectedMessage), default)).MustHaveHappened();
+        }
+
+        [Fact]
+        public async Task TransmitUnsubscribeAsync_Transmits_Correct_Unsubscribe_Request()
+        {
+            var topics = new[] { "topic-1", "topic-2" };
+            var packetId = (ushort)0x8888;
+            var expectedMessage = new Unsubscribe(packetId, topics);
+
+            await messageSender.TransmitUnsubscribeAsync(packetId, topics, default);
+
+            A.CallTo(() => messageSender.SendAsync(new Transmit<Unsubscribe>(expectedMessage), default)).MustHaveHappened();
+        }
+
+        [Fact]
+        public async Task TransmitPublishAsync_Transmits_Correct_Publish_Packet()
+        {
+            var expectedMessage = new Publish(0x8080, Guid.NewGuid().ToString(), Guid.NewGuid().ToByteArray(), QosLevel.Qos1);
+
+            await messageSender.TransmitPublishAsync(expectedMessage.Id, expectedMessage.Topic, expectedMessage.Qos, expectedMessage.Message, false, false, default);
+
+            A.CallTo(() => messageSender.SendAsync(new Transmit<Publish>(expectedMessage), default)).MustHaveHappened();
+        }
+
+        [Fact]
+        public async Task TransmitPingAsync_Transmits_Ping_Packet()
+        {
+            await messageSender.TransmitPingReqAsync(default);
+
+            A.CallTo(() => messageSender.SendAsync(new Transmit<PingReq>(PingReq.Instance), default)).MustHaveHappened();
         }
     }
 }
