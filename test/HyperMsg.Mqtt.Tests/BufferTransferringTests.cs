f0feab96954fcc81523b7b7bf1feeb2b486c410b
Max Gunther <maxim.chasnik@gmail.com>
2021-05-27 11:51:13 +0300

    wip

---
+++ test/HyperMsg.Mqtt.Tests/BufferTransferringTests.cs
@@ -0,0 +1,35 @@
+﻿using HyperMsg.Mqtt.Packets;
+using HyperMsg.Mqtt.Serialization;
+using System;
+using Xunit;
+
+namespace HyperMsg.Mqtt
+{
+    public class BufferTransferringTests : ServiceHostFixture
+    {
+        public BufferTransferringTests() : base(services => services.AddMqttServices())
+        { }
+
+        [Fact]
+        public void Serializes_Mqtt_Messages_Into_Transmitting_Buffer()
+        {
+            var expectedMessage = new Connect
+            {
+                ClientId = Guid.NewGuid().ToString()                
+            };
+            var actualMessage = default(object);
+
+            HandlersRegistry.RegisterBufferFlushReader(BufferType.Transmitting, data =>
+            {
+                (var bytesConsumed, var message) = MqttDeserializer.Deserialize(data);
+                actualMessage = message;
+                return bytesConsumed;
+            });
+
+            MessageSender.SendTransmitMessageCommand(expectedMessage);
+
+            Assert.NotNull(actualMessage);
+            Assert.Equal(expectedMessage, actualMessage);
+        }
+    }
+}
