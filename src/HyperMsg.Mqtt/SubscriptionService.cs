cf367737f9f3e98d3bda49c9301bde7053e65860
Maxim <maxim.chasnik@gmail.com>
2022-11-09 16:15:00 +0200

    wip

---
+++ src/HyperMsg.Mqtt/SubscriptionService.cs
@@ -0,0 +1,73 @@
+﻿using HyperMsg.Mqtt.Packets;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HyperMsg.Mqtt;
+
+public class SubscriptionService : IDisposable
+{
+    private readonly IDispatcher dispatcher;
+    private readonly IRegistry registry;
+    private readonly ConcurrentDictionary<ushort, Subscribe> requestedSubscriptions;
+
+    public SubscriptionService(IDispatcher dispatcher, IRegistry registry)
+    {
+        this.dispatcher = dispatcher;
+        this.registry = registry;
+        requestedSubscriptions = new();
+        RegisterHandlers(this.registry);
+    }
+
+    private void RegisterHandlers(IRegistry registry)
+    {
+        registry.Register<SubAck>(HandleSubscriptionResponse);
+        registry.Register<UnsubAck>(HandleUnsubAckResponse);
+    }
+
+    private void DeregisterHandlers(IRegistry registry)
+    {
+        registry.Deregister<SubAck>(HandleSubscriptionResponse);
+        registry.Deregister<UnsubAck>(HandleUnsubAckResponse);
+    }
+
+    public ushort RequestSubscription(params SubscriptionRequest[] subscriptions) => RequestSubscription((IEnumerable<SubscriptionRequest>)subscriptions);
+
+    public ushort RequestSubscription(IEnumerable<SubscriptionRequest> subscriptions)
+    {
+        var request = new Subscribe(PacketId.New(), subscriptions);
+        requestedSubscriptions.AddOrUpdate(request.Id, request, (id, val) => val);
+        dispatcher.Dispatch(request);
+        return request.Id;
+    }
+
+    public ushort RequestUnsubscription(params string[] topicNames) => RequestUnsubscription((IEnumerable<string>)topicNames);
+
+    public ushort RequestUnsubscription(IEnumerable<string> topicNames)
+    {
+        var request = new Unsubscribe(PacketId.New(), topicNames);
+
+        dispatcher.Dispatch(request);
+        return request.Id;
+    }
+
+    private void HandleSubscriptionResponse(SubAck response)
+    {
+        if (!requestedSubscriptions.TryGetValue(response.Id, out var request))
+        {
+            return;
+        }
+
+        var requestedTopics = request.Subscriptions.Select(s => s.TopicName).ToArray();
+                
+        requestedSubscriptions.Remove(response.Id, out _);
+    }
+
+    private void HandleUnsubAckResponse(UnsubAck unsubAck)
+    {
+        
+    }
+
+    public void Dispose() => DeregisterHandlers(registry);
+}
---
+++ test/HyperMsg.Mqtt.Tests/SubscriptionServiceTests.cs
@@ -0,0 +1,67 @@
+﻿using HyperMsg.Mqtt.Packets;
+using System.Linq;
+using Xunit;
+
+namespace HyperMsg.Mqtt;
+
+public class SubscriptionServiceTests
+{
+    private readonly MessageBroker messageBroker;
+    private readonly SubscriptionService session;
+
+    public SubscriptionServiceTests()
+    {
+        messageBroker = new();
+        session= new(messageBroker, messageBroker);
+    }
+
+    [Fact]
+    public void RequestSubscription_Sends_Correct_Subscribe_Request()
+    {
+        var subscribePacket = default(Subscribe);
+        messageBroker.Register<Subscribe>(subscribe => subscribePacket = subscribe);
+        var request = Enumerable.Range(1, 5)
+            .Select(i => new SubscriptionRequest($"topic-{i}", (QosLevel)(i % 3)))
+            .ToArray();
+
+        var packetId = session.RequestSubscription(request);
+
+        Assert.NotNull(subscribePacket);
+        Assert.Equal(packetId, subscribePacket.Id);
+        //Assert.True(requestStorage.Contains<Subscribe>(packetId));
+    }
+
+    [Fact]
+    public void SubAck_Response_Invokes_Handler_Registered_With_RegisterSubscriptionResponseHandler()
+    {
+        var actualResult = default(SubscriptionResponseHandlerArgs);
+
+        var request = Enumerable.Range(1, 5)
+            .Select(i => new SubscriptionRequest($"topic-{i}", (QosLevel)(i % 3)))
+            .ToArray();
+
+        messageBroker.Register<SubscriptionResponseHandlerArgs>(response => actualResult = response);
+        var packetId = session.RequestSubscription(request);
+        var subAck = new SubAck(packetId, new[] { SubscriptionResult.Failure, SubscriptionResult.SuccessQos1, SubscriptionResult.SuccessQos0 });
+        messageBroker.Dispatch(subAck);
+
+        Assert.NotNull(actualResult);
+        //Assert.False(requestStorage.Contains<Subscribe>(packetId));
+        Assert.Equal(subAck.Results, actualResult.SubscriptionResults);
+    }
+
+    [Fact]
+    public void RequestUnsubscription_Sends_Unsubscription_Request()
+    {
+        var unsubscribe = default(Unsubscribe);
+        messageBroker.Register<Unsubscribe>(packet => unsubscribe = packet);
+        var topics = new[] { "topic-1", "topic-2" };
+
+        var packetId = messageBroker.SendUnsubscribeRequest(topics);
+
+        Assert.NotNull(unsubscribe);
+        Assert.Equal(packetId, unsubscribe.Id);
+        Assert.Equal(topics, unsubscribe.Topics);
+        //Assert.True(requestStorage.Contains<Unsubscribe>(unsubscribe.Id));
+    }
+}
