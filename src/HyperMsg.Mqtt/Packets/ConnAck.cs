3425704064d723cee2fe5d4c576746a11f4849fb
Maxim <maxim.chasnik@gmail.com>
2023-05-09 15:50:10 +0300

    Added Packet type

--- src/HyperMsg.Mqtt/Packets/ConnAck.cs
+++ src/HyperMsg.Mqtt/Packets/ConnAck.cs
@@ -2,5 +2,7 @@
 
 public record ConnAck(ConnectReasonCode ReasonCode, bool SessionPresent = false, ConnAckProperties Properties = null)
 {
+    internal Packet ToPacket() => new(PacketType.ConAck, this);
+
     public override string ToString() => $"ConnAck(SP={SessionPresent},Code={ReasonCode})";
 }
---
+++ src/HyperMsg.Mqtt/Packets/Packet.cs
@@ -0,0 +1,28 @@
+﻿namespace HyperMsg.Mqtt.Packets;
+
+public readonly struct Packet
+{
+    private readonly object packet;
+
+    internal Packet(PacketType type, object packet)
+    {
+        this.packet = packet;
+        Type = type;
+    }
+
+    public PacketType Type { get; }
+
+    public bool IsConnect => Type == PacketType.Connect;
+
+    public bool IsConnAck => Type == PacketType.ConAck;
+
+    public bool IsPublish => Type == PacketType.Publish;
+
+    public Connect ToConnect() => To<Connect>();
+
+    public ConnAck ToConnAck() => To<ConnAck>();
+
+    public Publish ToPublish() => To<Publish>();
+
+    private T To<T>() => (T)packet;
+}
