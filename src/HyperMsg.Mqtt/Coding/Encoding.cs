c875ee6f7a54785118a97b42bb6b808e0b6165d4
Maxim <maxim.chasnik@gmail.com>
2023-05-25 12:38:13 +0300

    Extracted Encoding for Connect packet into separate file

--- src/HyperMsg.Mqtt/Coding/Encoding.cs
+++ src/HyperMsg.Mqtt/Coding/Encoding.cs
@@ -4,75 +4,12 @@ using System.Buffers.Binary;
 
 namespace HyperMsg.Mqtt.Coding;
 
-public static class Encoding
+public static partial class Encoding
 {
     private static readonly byte[] Disconnect = { 0b11100000, 0b00000000 };
     private static readonly byte[] PingResp = { 0b11010000, 0b00000000 };
     private static readonly byte[] PingReq = { 0b11000000, 0b00000000 };
     private static readonly byte[] ProtocolName = { 0, 4, (byte)'M', (byte)'Q', (byte)'T', (byte)'T', 4 };
-
-    const byte ConnectCode = 0b00010000;
-
-    public static void Encode(IBufferWriter writer, Connect connect)
-    {
-        var contentLength = 10 + GetStringByteCount(connect.ClientId);
-
-        if (connect.Flags.HasFlag(ConnectFlags.Will))
-        {
-            contentLength += GetStringByteCount(connect.WillTopic);
-            contentLength += connect.WillPayload.Length + 2;
-        }
-
-        if (connect.Flags.HasFlag(ConnectFlags.UserName))
-        {
-            contentLength += GetStringByteCount(connect.UserName);
-        }
-
-        if (connect.Flags.HasFlag(ConnectFlags.Password))
-        {
-            contentLength += connect.Password.Length + 2;
-        }
-
-        var buffer = writer.GetMemory(contentLength);
-        var span = buffer.Span;
-
-        span[0] = ConnectCode;
-        int written = buffer.Span[1..].WriteVarInt(contentLength);
-        writer.Advance(written + 1);
-
-        writer.Write(ProtocolName);
-        span = writer.GetSpan(3);
-        span[0] = (byte)connect.Flags;
-        BinaryPrimitives.WriteUInt16BigEndian(span[1..], connect.KeepAlive);
-        writer.Advance(3);
-
-        written = writer.WriteString(connect.ClientId);
-        writer.Advance(written);
-
-        if (connect.Flags.HasFlag(ConnectFlags.Will))
-        {
-            written = writer.WriteString(connect.WillTopic);
-            writer.Advance(written);
-            span = writer.GetSpan(connect.WillPayload.Length + sizeof(ushort));
-            BinaryPrimitives.WriteUInt16BigEndian(span, (ushort)connect.WillPayload.Length);
-            connect.WillPayload.Span.CopyTo(span[2..]);
-            writer.Advance(connect.WillPayload.Length + 2);
-        }
-
-        if (connect.Flags.HasFlag(ConnectFlags.UserName))
-        {
-            written = writer.WriteString(connect.UserName);
-            writer.Advance(written);
-        }
-
-        if (connect.Flags.HasFlag(ConnectFlags.Password))
-        {
-            span = writer.GetSpan(connect.Password.Length + 2);
-            BinaryPrimitives.WriteUInt16BigEndian(span, (ushort)connect.Password.Length);
-            //connect.Password.CopyTo(span[2..]);
-            writer.Advance(connect.Password.Length + 2);
-        }
-    }
 
     public static void Encode(IBufferWriter writer, ConnAck connAck)
     {
---
+++ test/HyperMsg.Mqtt.Tests/Coding/ConnectEncodingTests.cs
@@ -0,0 +1,141 @@
+﻿using HyperMsg.Mqtt.Packets;
+using System.Buffers;
+using Xunit;
+
+namespace HyperMsg.Mqtt.Coding;
+
+public class ConnectEncodingTests
+{
+    private readonly Buffer buffer;
+    private readonly IBufferWriter bufferWriter;
+
+    [Fact(DisplayName = "Encodes Connect packet with Password flag")]
+    public void Encode_Connect_With_Password_Flag()
+    {
+        byte[] password = Guid.NewGuid().ToByteArray();
+        var packet = new Connect
+        {
+            Flags = ConnectFlags.Password,
+            ClientId = Guid.NewGuid().ToString(),
+            Password = password
+        };
+        var expected = CreateConnectHeader(packet);
+        AddBinary(expected, password);
+        SetRemainingLength(expected);
+
+        Encoding.Encode(bufferWriter, packet);
+
+        VerifySerialization(expected.ToArray());
+    }
+
+    [Fact(DisplayName = "Encode Connect with Username flag")]
+    public void Encode_Connect_With_Username_Flag()
+    {
+        string username = Guid.NewGuid().ToString();
+        var packet = new Connect
+        {
+            ClientId = Guid.NewGuid().ToString(),
+            Flags = ConnectFlags.UserName,
+            UserName = username
+        };
+        var expected = CreateConnectHeader(packet);
+        AddString(expected, username);
+        SetRemainingLength(expected);
+
+        Encoding.Encode(bufferWriter, packet);
+
+        VerifySerialization(expected.ToArray());
+    }
+
+    [Fact(DisplayName = "Encodes Connect with Will flag")]
+    public void Encode_Connect_With_Will_Flag()
+    {
+        string willTopic = Guid.NewGuid().ToString();
+        byte[] willMessage = Guid.NewGuid().ToByteArray();
+        var packet = new Connect
+        {
+            ClientId = Guid.NewGuid().ToString(),
+            Flags = ConnectFlags.Will,
+            WillTopic = willTopic,
+            WillPayload = willMessage
+        };
+        var expected = CreateConnectHeader(packet);
+        AddString(expected, willTopic);
+        AddBinary(expected, willMessage);
+        SetRemainingLength(expected);
+
+        Encoding.Encode(bufferWriter, packet);
+
+        VerifySerialization(expected.ToArray());
+    }
+
+    public static IEnumerable<object[]> GetTestCasesForConnectEncoding()
+    {
+        yield return new object[] { ConnectFlags.None };
+        yield return new object[] { ConnectFlags.CleanSession };
+        yield return new object[] { ConnectFlags.WillRetain };
+        yield return new object[] { ConnectFlags.Qos0 };
+        yield return new object[] { ConnectFlags.Qos1 };
+        yield return new object[] { ConnectFlags.Qos2 };
+        yield return new object[] { ConnectFlags.CleanSession | ConnectFlags.WillRetain };
+    }
+
+    [Theory(DisplayName = "Encodes Connect packet")]
+    [MemberData(nameof(GetTestCasesForConnectEncoding))]
+    public void Encode_Connect(ConnectFlags flags)
+    {
+        ushort keepAlive = BitConverter.ToUInt16(Guid.NewGuid().ToByteArray(), 0);
+        string clientId = Guid.NewGuid().ToString();
+        var packet = new Connect
+        {
+            Flags = flags,
+            KeepAlive = keepAlive,
+            ClientId = clientId
+        };
+        var expected = CreateConnectHeader(packet);
+        SetRemainingLength(expected);
+
+        Encoding.Encode(bufferWriter, packet);
+
+        VerifySerialization(expected.ToArray());
+    }
+
+    private static List<byte> CreateConnectHeader(Connect packet)
+    {
+        List<byte> expected = new List<byte>
+        {
+            0x10, //Type code
+				0, //Length placeholder
+				0, 4, (byte)'M', (byte)'Q', (byte)'T', (byte)'T', //Protocol name
+				4, //Protocol level
+				(byte)packet.Flags, //Flags
+				(byte)(packet.KeepAlive >> 8), (byte)packet.KeepAlive,
+				//0, (byte)packet.ClientId.Length//Client ID length
+			};
+        AddString(expected, packet.ClientId);
+        return expected;
+    }
+
+    private static void AddBinary(List<byte> packet, byte[] bytes)
+    {
+        packet.AddRange(new byte[] { 0, (byte)bytes.Length });
+        packet.AddRange(bytes);
+    }
+
+    private static void AddString(List<byte> packet, string str)
+    {
+        packet.AddRange(new byte[] { 0, (byte)str.Length });
+        packet.AddRange(System.Text.Encoding.UTF8.GetBytes(str));
+    }
+
+    private static void SetRemainingLength(List<byte> packet)
+    {
+        packet[1] = (byte)(packet.Count - 2);
+    }
+
+    private void VerifySerialization(params byte[] expected)
+    {
+        var actual = buffer.Reader.GetMemory().ToArray();
+        Assert.Equal(expected, actual);
+    }
+}
