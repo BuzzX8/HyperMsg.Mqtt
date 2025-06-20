9a2beca965779dc1878d918610170befbe1f3bdb
BuzzX8 <maxim.chasnik@gmail.com>
2025-06-20 21:26:52 +0300

    Added EncodingComponent

---
+++ src/HyperMsg.Mqtt/Coding/EncodingComponent.cs
@@ -0,0 +1,35 @@
+﻿using HyperMsg.Buffers;
+using HyperMsg.Messaging;
+using HyperMsg.Mqtt.Packets;
+
+namespace HyperMsg.Mqtt.Coding;
+
+public class EncodingComponent : IMessagingComponent
+{
+    IBufferWriter _bufferWriter;
+
+    public EncodingComponent(IBufferWriter bufferWriter)
+    {
+        _bufferWriter = bufferWriter;
+    }
+
+    public void Attach(IMessagingContext messagingContext)
+    {
+        ArgumentNullException.ThrowIfNull(messagingContext);
+
+        messagingContext.HandlerRegistry.Register<Packet>(Handle);
+    }
+
+    public void Detach(IMessagingContext messagingContext)
+    {
+        ArgumentNullException.ThrowIfNull(messagingContext);
+
+        messagingContext.HandlerRegistry.Unregister<Packet>(Handle);
+    }
+
+    public void Handle(Packet packet)
+    {
+        Encoding.Encode(_bufferWriter.GetSpan(), packet, out int bytesWritten);
+        _bufferWriter.Advance(bytesWritten);
+    }
+}
---
+++ src/HyperMsg.Mqtt/Coding/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HyperMsg.Mqtt.Coding;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddMqttCoding(this IServiceCollection services)
+    {
+        
+        return services;
+    }
+}
