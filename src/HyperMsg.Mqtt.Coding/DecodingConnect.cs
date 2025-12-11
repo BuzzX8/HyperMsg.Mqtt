0258abe53cd3777e6ec3cd34bb800938bb61abe6
Max Gunther <maxim.chasnik@gmail.com>
2025-12-11 10:58:18 +0200

    Connect packet decoding update (#56)

--- src/HyperMsg.Mqtt.Coding/DecodingConnect.cs
+++ src/HyperMsg.Mqtt.Coding/DecodingConnect.cs
@@ -1,4 +1,5 @@
-﻿using HyperMsg.Mqtt.Packets;
+﻿using HyperMsg.Coding;
+using HyperMsg.Mqtt.Packets;
 
 namespace HyperMsg.Mqtt.Coding;
 
@@ -6,30 +7,30 @@ public static partial class Decoding
 {
     private static readonly Dictionary<byte, PropertyUpdater<ConnectProperties>> ConnectPropertyUpdaters = new()
     {
-        [0x11] = (ConnectProperties p, ReadOnlySpan<byte> b, ref int offset) => p.SessionExpiryInterval = b.ReadUInt32(ref offset),
-        [0x15] = (ConnectProperties p, ReadOnlySpan<byte> b, ref int offset) => p.AuthenticationMethod = b.ReadString(ref offset),
-        [0x16] = (ConnectProperties p, ReadOnlySpan<byte> b, ref int offset) => p.AuthenticationData = b.ReadBinaryData(ref offset),
-        [0x17] = (ConnectProperties p, ReadOnlySpan<byte> b, ref int offset) => p.RequestProblemInformation = b.ReadBoolean(ref offset),
-        [0x19] = (ConnectProperties p, ReadOnlySpan<byte> b, ref int offset) => p.RequestResponseInformation = b.ReadBoolean(ref offset),
-        [0x21] = (ConnectProperties p, ReadOnlySpan<byte> b, ref int offset) => p.ReceiveMaximum = b.ReadUInt16(ref offset),
-        [0x22] = (ConnectProperties p, ReadOnlySpan<byte> b, ref int offset) => p.TopicAliasMaximum = b.ReadUInt16(ref offset),
-        [0x26] = (ConnectProperties p, ReadOnlySpan<byte> b, ref int offset) =>
+        [0x11] = (p, b, ref offset) => p.SessionExpiryInterval = b.ReadUInt32(ref offset),
+        [0x15] = (p, b, ref offset) => p.AuthenticationMethod = b.ReadString(ref offset),
+        [0x16] = (p, b, ref offset) => p.AuthenticationData = b.ReadBinaryData(ref offset),
+        [0x17] = (p, b, ref offset) => p.RequestProblemInformation = b.ReadBoolean(ref offset),
+        [0x19] = (p, b, ref offset) => p.RequestResponseInformation = b.ReadBoolean(ref offset),
+        [0x21] = (p, b, ref offset) => p.ReceiveMaximum = b.ReadUInt16(ref offset),
+        [0x22] = (p, b, ref offset) => p.TopicAliasMaximum = b.ReadUInt16(ref offset),
+        [0x26] = (p, b, ref offset) =>
         {
             p.UserProperties ??= new Dictionary<string, string>();
             ReadUserProperty(p.UserProperties, b, ref offset);
         },
-        [0x27] = (ConnectProperties p, ReadOnlySpan<byte> b, ref int offset) => p.MaximumPacketSize = b.ReadUInt32(ref offset),
+        [0x27] = (p, b, ref offset) => p.MaximumPacketSize = b.ReadUInt32(ref offset),
     };
 
     private static readonly Dictionary<byte, PropertyUpdater<ConnectWillProperties>> ConnectWillPropertyUpdaters = new()
     {
-        [0x01] = (ConnectWillProperties p, ReadOnlySpan<byte> b, ref int offset) => p.PayloadFormatIndicator = b.ReadByte(ref offset),
-        [0x02] = (ConnectWillProperties p, ReadOnlySpan<byte> b, ref int offset) => p.MessageExpiryInterval = b.ReadUInt32(ref offset),
-        [0x03] = (ConnectWillProperties p, ReadOnlySpan<byte> b, ref int offset) => p.ContentType = b.ReadString(ref offset),
-        [0x08] = (ConnectWillProperties p, ReadOnlySpan<byte> b, ref int offset) => p.ResponseTopic = b.ReadString(ref offset),
-        [0x09] = (ConnectWillProperties p, ReadOnlySpan<byte> b, ref int offset) => p.CorrelationData = b.ReadBinaryData(ref offset),
-        [0x18] = (ConnectWillProperties p, ReadOnlySpan<byte> b, ref int offset) => p.WillDelayInterval = b.ReadUInt32(ref offset),
-        [0x26] = (ConnectWillProperties p, ReadOnlySpan<byte> b, ref int offset) =>
+        [0x01] = (p, b, ref offset) => p.PayloadFormatIndicator = b.ReadByte(ref offset),
+        [0x02] = (p, b, ref offset) => p.MessageExpiryInterval = b.ReadUInt32(ref offset),
+        [0x03] = (p, b, ref offset) => p.ContentType = b.ReadString(ref offset),
+        [0x08] = (p, b, ref offset) => p.ResponseTopic = b.ReadString(ref offset),
+        [0x09] = (p, b, ref offset) => p.CorrelationData = b.ReadBinaryData(ref offset),
+        [0x18] = (p, b, ref offset) => p.WillDelayInterval = b.ReadUInt32(ref offset),
+        [0x26] = (p, b, ref offset) =>
         {
             p.UserProperties ??= new Dictionary<string, string>();
             ReadUserProperty(p.UserProperties, b, ref offset);
@@ -42,8 +43,8 @@ public static partial class Decoding
         var packetSize = buffer.ReadVarInt(ref offset);
 
         //Variable header
-        var protocolName = buffer.ReadString(ref offset);
-        var protocolVersion = buffer.ReadByte(ref offset);
+        VerifyProtocolName(buffer, ref offset);
+        var protocolVersion = (ProtocolVersion)buffer.ReadByte(ref offset);
         var flags = (ConnectFlags)buffer.ReadByte(ref offset);
         var keepAlive = buffer.ReadUInt16(ref offset);
         var properties = DecodeConnectProperties(buffer, protocolVersion, ref offset);
@@ -74,9 +75,18 @@ public static partial class Decoding
         return connect;
     }
 
-    private static ConnectProperties DecodeConnectProperties(ReadOnlySpan<byte> buffer, byte protocolVersion, ref int offset)
+    private static void VerifyProtocolName(ReadOnlySpan<byte> buffer, ref int offset)
     {
-        if (protocolVersion < 5)
+        var protocolName = buffer.ReadString(ref offset);
+        if (protocolName != "MQTT")
+        {
+            throw new DecodingException("Invalid protocol name");
+        }
+    }
+
+    private static ConnectProperties? DecodeConnectProperties(ReadOnlySpan<byte> buffer, ProtocolVersion protocolVersion, ref int offset)
+    {
+        if (protocolVersion != ProtocolVersion.V5_0)
         {
             return default;
         }
@@ -91,9 +101,9 @@ public static partial class Decoding
         connect.WillPayload = buffer.ReadBinaryData(ref offset);
     }
 
-    private static ConnectWillProperties DecodeWillProperties(ReadOnlySpan<byte> buffer, byte protocolVersion, ref int offset)
+    private static ConnectWillProperties? DecodeWillProperties(ReadOnlySpan<byte> buffer, ProtocolVersion protocolVersion, ref int offset)
     {
-        if (protocolVersion < 5)
+        if (protocolVersion != ProtocolVersion.V5_0)
         {
             return default;
         }
---
+++ test/HyperMsg.Mqtt.Coding.Tests/ConnectDecodingTestData.cs
@@ -0,0 +1,63 @@
+﻿namespace HyperMsg.Mqtt.Coding;
+
+internal class ConnectDecodingTestData
+{
+    // Minimal valid CONNECT packet (MQTT v5, empty Client ID)
+    public static readonly byte[] CorrectPacketWithEmptyClientId =
+    [
+        0x10,       // Fixed header: CONNECT
+        0x0B,       // Remaining length = 11
+        0x00, 0x04, // Protocol Name length
+        0x4D, 0x51, 0x54, 0x54, // "MQTT"
+        0x05,       // Protocol Level = 5
+        0x02,       // Connect Flags (Clean Start)
+        0x00, 0x3C, // Keep Alive = 60
+        0x00,       // Properties length = 0
+        0x00, 0x00  // Client ID length = 0 (empty)
+    ];
+
+    // Minimal valid CONNECT packet (MQTT v5, Client ID = "c")
+    public static readonly byte[] CorrectPacketMinimal =
+    [
+        0x10,       // Fixed header: CONNECT
+        0x0C,       // Remaining length = 12
+        0x00, 0x04, // Protocol Name length
+        0x4D, 0x51, 0x54, 0x54, // "MQTT"
+        0x05,       // Protocol Level = 5
+        0x02,       // Connect Flags (Clean Start)
+        0x00, 0x3C, // Keep Alive = 60
+        0x00,       // Properties length = 0
+        0x00, 0x01, // Client ID length = 1
+        0x63        // "c"
+    ];
+
+    // CONNECT packet with various properties and payload fields set
+    public static readonly byte[] CorrectPacketWithProperties =
+    [
+        16, 171, 1, //Fixed header
+        0, 4, 77, 81, 84, 84, //Protocol name (MQTT)
+        5, //Protocol version
+        198, //Flags
+        0, 60, //Keep alive
+        //Properties
+        48, 17, 0, 0, 0, 11, 33, 117, 48, 39, 0, 0, 136, 184, 34, 0, 12, 25, 1, 23, 1, 38, 0, 5, 80, 114, 111, 112, 49, 0, 4, 86, 97, 108, 49, 38, 0, 5, 80, 114, 111, 112, 50, 0, 4, 86, 97, 108, 50, 
+        //Payload
+        0, 14, 109, 113, 116, 116, 120, 95, 52, 98, 57, 52, 50, 54, 55, 102, //Client ID
+        51, 24, 0, 0, 0, 45, 1, 0, 2, 0, 0, 0, 26, 3, 0, 9, 115, 111, 109, 101, 45, 116, 121, 112, 101, 8, 0, 14, 114, 101, 115, 112, 111, 110, 115, 101, 45, 116, 111, 112, 105, 99, 9, 0, 7, 53, 50, 51, 53, 52, 51, 52, 0, 15, 108, 97, 115, 116, 45, 119, 105, 108, 108, 45, 116, 111, 112, 105, 99, 0, 0, 0, 8, 74, 111, 104, 110, 32, 68, 111, 101, 0, 13, 115, 111, 109, 101, 45, 112, 97, 115, 115, 119, 111, 114, 100
+    ];
+
+    // CONNECT packet with incorrect protocol name ("MQTS" instead of "MQTT")
+    public static readonly byte[] InvalidProtocolName =
+    [
+        0x10,       // Fixed header: CONNECT
+        0x12,       // Remaining length = 18
+        0x00, 0x04, // Protocol Name length
+        0x4D, 0x51, 0x54, 0x53, // "MQTS" (invalid)
+        0x05,       // Protocol Level = 5
+        0x02,       // Connect Flags (Clean Start)
+        0x00, 0x3C, // Keep Alive = 60
+        0x00,       // Properties length = 0
+        0x00, 0x0A, // Client ID length = 10
+        0x74, 0x65, 0x73, 0x74, 0x43, 0x6C, 0x69, 0x65, 0x6E, 0x74 // "testClient"
+    ];
+}
