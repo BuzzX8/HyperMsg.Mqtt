e222f9c8ab1076821acfec4ed224a0de8ff8598a
BuzzX8 <maxim.chasnik@gmail.com>
2025-12-26 03:38:53 +0200

    wip

---
+++ test/HyperMsg.Mqtt.Client.Tests/MqttClientTests.cs
@@ -0,0 +1,14 @@
+﻿using FakeItEasy;
+
+namespace HyperMsg.Mqtt.Client;
+
+public class MqttClientTests
+{
+    private readonly MqttClient _mqttClient;
+    private readonly IClientContext _clientContext;
+
+    public MqttClientTests()
+    {
+        _clientContext = A.Fake<IClientContext>();
+    }
+}
