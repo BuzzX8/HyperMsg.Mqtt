0e280db35746bf225b078e951b778040a4c50e43
BuzzX8 <maxim.chasnik@gmail.com>
2025-12-26 05:14:26 +0200

    wip

--- test/HyperMsg.Mqtt.Client.Tests/MqttClientTests.cs
+++ test/HyperMsg.Mqtt.Client.Tests/MqttClientTests.cs
@@ -1,4 +1,6 @@
 ﻿using FakeItEasy;
+using HyperMsg.Transport;
+using Xunit;
 
 namespace HyperMsg.Mqtt.Client;
 
@@ -6,9 +8,25 @@ public class MqttClientTests
 {
     private readonly MqttClient _mqttClient;
     private readonly IClientContext _clientContext;
+    private readonly ConnectionSettings _connectionSettings;
 
     public MqttClientTests()
     {
         _clientContext = A.Fake<IClientContext>();
+        _connectionSettings = new ConnectionSettings(Guid.NewGuid().ToString());
+        _mqttClient = new MqttClient(_clientContext, _connectionSettings);
+    }
+
+    [Fact]
+    public async Task ConnectAsync_ShouldInvokeConnectionConnectAsync()
+    {
+        // Arrange
+        var cancellationToken = CancellationToken.None;
+        var connection = A.Fake<IConnection>();
+        A.CallTo(() => _clientContext.Connection).Returns(connection);
+        // Act
+        await _mqttClient.ConnectAsync(cancellationToken);
+        // Assert
+        A.CallTo(() => connection.OpenAsync(cancellationToken)).MustHaveHappenedOnceExactly();
     }
 }
